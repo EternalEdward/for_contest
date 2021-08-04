deec0205b34d0384339217f054508a00f20c7d94
21-084-20 <21-084-20@example.com>
2021-08-05 07:15:03 +0800

    Upload New File

---
+++ thinpad_top.srcs/sources_1/new/CTRL.v
@@ -0,0 +1,21 @@
+module CTRL (
+    input rst,
+    input pasue_from_id,
+    input pasue_from_exe,
+    input pasue_from_mem,
+    output reg [5:0]stall
+);
+//0     1     2   3   4   5 
+//PC    IF    ID  EXE MEM WB
+always @(*) begin
+    if(rst)begin
+        stall <= 6'b000000;
+    end else begin
+        stall <= //pasue_from_mem ?   6'b000111 :
+                 pasue_from_exe ?   6'b001111 :
+                 pasue_from_id  ?   6'b000011 :
+                                    6'b000000;
+    end
+end
+    
+endmodule
